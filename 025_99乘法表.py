15abbe6c634b77a7af068743a028f9229b1100da
niejian <niejian@bluemoon.com.cn>
2024-01-04 16:35:55 +0800

    更新

--- 025_99乘法表.py
+++ 025_99乘法表.py
@@ -5,7 +5,8 @@
 # @project: python_basic2
 
 data = [1,2,3,4,5,6,7,8,9]
-for i in data:
+# for i in data:
+for i in range(1, 10):
     j = 1
     while j <= i:
         print("%d * %d = %d" % (i, j, i * j), end="\t") # 不换行打印
--- 026_打印三角形.py
+++ 026_打印三角形.py
@@ -6,6 +6,21 @@
 
 for i in range(10):
     if i <= 5:
-        print("*" * i)
+        print("* " * i)
     else:
-        print("*" * (10-i))
+        print("* " * (10-i))
+
+
+# 打印等腰三角形
+level = input("请输入层级：")
+level = int(level)
+while level <= 0:
+    print("层级必须大于0")
+i = 1
+while i <= level:
+    # 打印空格 level - i
+    print(" " * (level - i), end="")
+    print("* " * i)
+    i += 1
+
+
